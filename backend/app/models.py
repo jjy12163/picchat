f114a66a9e1b00b612d1a26c9a50437f1d6779b2
ryu <rhyunju31@gmail.com>
2024-07-29 08:21:08 +0900

    Feat: DB 테이블 및 속성 추가
    user 테이블 프로필 이미지 속성 추가 및 비밀번호 속성 제거
    faceimage 테이블 추가

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,15 +5,15 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column('idUser', db.Integer, primary_key=True)
     username = db.Column('userName', db.String(45), nullable=False)
-    password = db.Column(db.String(45), nullable=False)
     email = db.Column(db.String(45), nullable=False)
     nickname = db.Column(db.String(45), nullable=True)
+    profile_image = db.Column(db.String(255), nullable=True)
     faceimages = db.relationship('FaceImage', backref='user', lazy=True)
 
 class FaceImage(db.Model):
     __tablename__ = 'faceimage'
     id = db.Column(db.Integer, primary_key=True)
-    image = db.Column(db.LargeBinary, nullable=True)
+    image = db.Column(db.LargeBinary(length=(2**24)-1), nullable=True)
     filename = db.Column(db.String(255), nullable=False)
     uploaded_at = db.Column(db.DateTime, server_default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.idUser'), nullable=False)
